7fc4cda44d82fc32acfc23d8a3a3588e2dddcd1c
Guna Sekhar Reddy <95278848+VU21CSEN0100380@users.noreply.github.com>
2026-01-13 17:04:32 +0530

    Update main.tf

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,1 +1,18 @@
+resource "aws_vpc" "this" {
+  cidr_block = var.vpc_cidr
+}
+
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.this.id
+  cidr_block              = var.subnet_cidr
+  map_public_ip_on_launch = true
+}
+
+output "vpc_id" {
+  value = aws_vpc.this.id
+}
+
+output "subnet_id" {
+  value = aws_subnet.public.id
+}
 
