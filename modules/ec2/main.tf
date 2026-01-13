e6bf406ae74d738d7ecb9289a99be793b1c1f9fe
Guna Sekhar Reddy <95278848+VU21CSEN0100380@users.noreply.github.com>
2026-01-13 17:05:01 +0530

    Update main.tf

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,1 +1,31 @@
+resource "aws_security_group" "web_sg" {
+  vpc_id = var.vpc_id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "web" {
+  ami           = var.ami
+  instance_type = var.instance_type
+  subnet_id     = var.subnet_id
+  security_groups = [aws_security_group.web_sg.id]
+
+  user_data = file("userdata.sh")
+
+  tags = {
+    Name = "Terraform-Docker-App"
+  }
+}
 
