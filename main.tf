a57a6a09a1aec02eadafc146fe401c40dfcff29e
Guna Sekhar Reddy <95278848+VU21CSEN0100380@users.noreply.github.com>
2026-01-13 17:12:00 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,14 @@
+module "vpc" {
+  source       = "./modules/vpc"
+  vpc_cidr     = var.vpc_cidr
+  subnet_cidr  = var.subnet_cidr
+}
+
+module "ec2" {
+  source        = "./modules/ec2"
+  vpc_id        = module.vpc.vpc_id
+  subnet_id     = module.vpc.subnet_id
+  ami           = var.ami
+  instance_type = var.instance_type
+}
 
